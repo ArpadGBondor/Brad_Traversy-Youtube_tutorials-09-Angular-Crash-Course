53084911c7d25dd1892bca99d6a2e16fb9371d56
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2024-05-27 18:04:13 +0100

    fix: convert project to Angular 12

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '08-angular-crash-course' title`, () => {
+  it(`should have as title 'angular-crash'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('08-angular-crash-course');
+    expect(app.title).toEqual('angular-crash');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 08-angular-crash-course');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'angular-crash app is running!'
+    );
   });
 });
